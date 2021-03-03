aada836031a0a77dd907fbd369f20302da6b40c5
Maic Lopez Saenz <maic@lopsae.com>
2021-03-03 12:21:35 -0800

    separate Stroke and Segment to their own files

---
+++ src/makeStroke.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+module.exports = function makeStroke(rac) {
+
+  return class RacStroke {
+
+    static none = new RacStroke(null);
+
+    constructor(color = null, weight = 1) {
+      this.color = color;
+      this.weight = weight;
+    }
+
+    copy() {
+      let colorCopy = null;
+      if (this.color !== null) {
+        colorCopy = this.color.copy();
+      }
+      return new RacStroke(colorCopy, this.weight);
+    }
+
+    withWeight(weight) {
+      return new RacStroke(this.color, weight);
+    }
+
+    withAlpha(alpha) {
+      if (this.color === null) {
+        return new RacStroke(null, this.weight);
+      }
+
+      let newColor = this.color.withAlpha(alpha);
+      return new RacStroke(newColor, this.weight);
+    }
+
+    styleWithFill(fill) {
+      return new rac.Style(this, fill);
+    }
+
+  } // RacStroke
+
+} // makeStroke
+
