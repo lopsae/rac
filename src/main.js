1f3220f876d2417709ca9a0ff942f84c64501dcf
Maic Lopez Saenz <maic@lopsae.com>
2021-03-22 17:30:16 -0700

    test cleanup

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@
     // https://github.com/amdjs/amdjs-api/blob/master/AMD.md
     // https://requirejs.org/docs/whyamd.html
     // AMD. Register as an anonymous module.
-    console.log(`Loading RAC for AMD - define:${typeof define}`);
+
+    // console.log(`Loading RAC for AMD - define:${typeof define}`);
     define([], factory);
     return;
   }
@@ -15,13 +16,15 @@
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    console.log(`Loading RAC for Node - module:${typeof module}`);
+
+    // console.log(`Loading RAC for Node - module:${typeof module}`);
     module.exports = factory();
     return;
   }
 
   // Browser globals (root is window)
-  console.log(`Loading RAC into self - root:${typeof root}`);
+
+  // console.log(`Loading RAC into self - root:${typeof root}`);
   root.makeRac = factory();
 
 }(typeof self !== 'undefined' ? self : this, function () {
--- tests/test.js
+++ tests/test.js
@@ -1,36 +1,30 @@
 
-console.log(`✳️  tests running!`);
 
 const makeRac = require('rulerandcompass');
-console.log(`Loaded RAC - version:${makeRac.version}`);
+const rac = makeRac();
 
-// let fail = 1;
 
 test('RAC version', () => {
   expect(makeRac.version).toBeTruthy();
+  expect(rac.version).toBeTruthy();
+  expect(makeRac.version).toBe(rac.version);
 });
 
 
-// if (makeRac.version === undefined || makeRac.version === null) {
-//   process.exit(fail);
-// }
-
-let rac = makeRac();
-
 
 expect.extend({
-  equalsPoint(received, x, y) {
-    const pass = received.x == x && received.y == y;
+  equalsPoint(point, x, y) {
+    const pass = point.x == x && point.y == y;
     if (pass) {
       return {
         message: () =>
-          `passed maic`,
+          `expected point at (${point.x},${point.y}) not to equal (${x},${y})`,
         pass: true,
       };
     } else {
       return {
         message: () =>
-          `failed maic`,
+          `expected point at (${point.x},${point.y}) to equal (${x},${y})`,
         pass: false,
       };
     }
@@ -42,12 +36,7 @@ test('Point', () => {
   let point = new rac.Point(100, 100);
   expect(point.x).toBe(100);
   expect(point.y).toBe(100);
-  expect(point).equalsPoint(100, 100);
+  expect(point).equalsPoint(100, 101);
 });
 
-
-
-
-// console.log(`✅ tests finished!`);
-// process.exit(0);
 
