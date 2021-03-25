8fc4bde0f36eaddf1f2af054b7ec7b652961e772
Maic Lopez Saenz <maic@lopsae.com>
2021-03-25 12:33:15 -0700

    flatten utils.test

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -5,63 +5,60 @@ const Rac = require('rulerandcompass');
 const tools = require('./testTools');
 
 
-describe('Utils', () => {
-
-  test('assertExists', () => {
-    expect(() => {Rac.utils.assertExists();})
-      .not.toThrow();
-    expect(() => {Rac.utils.assertExists("one", "two");})
-      .not.toThrow();
-    expect(() => {Rac.utils.assertExists("one", null, "three");})
-      .toThrow('FailedAssert');
-  });
-
-
-  test('typeName', () => {
-    let Duck = class Duck {};
-    let duck = new Duck();
-
-    expect(Rac.utils.typeName(Duck))
-      .toBe('function:Duck');
-    expect(Rac.utils.typeName(duck))
-      .toBe('Duck');
-
-    let namedFunc = function named() {};
-    expect(Rac.utils.typeName(namedFunc))
-      .toBe('function:named');
-
-    let unnamedFunc = function() {};
-    expect(Rac.utils.typeName(unnamedFunc))
-      .toBe('function:unnamedFunc');
-
-    let anonymousFunc = () => {};
-    expect(Rac.utils.typeName(anonymousFunc))
-      .toBe('function:anonymousFunc');
-
-    expect(Rac.utils.typeName(() => {}))
-      .toBe('function');
-
-    expect(Rac.utils.typeName(55))
-      .toBe('Number');
-    expect(Rac.utils.typeName('string'))
-      .toBe('String');
-  });
-
-
-  test('addConstant', () => {
-    let obj = {};
-    expect(obj).not.toHaveProperty('prop');
-
-    Rac.utils.addConstant(obj, 'prop', 'value');
-    expect(obj).toHaveProperty('prop', 'value');
-
-    let propDesc = Object.getOwnPropertyDescriptor(obj, 'prop');
-    expect(propDesc.writable).toBe(false);
-
-    obj.assigned = 'other';
-    expect(obj).toHaveProperty('assigned', 'other');
-    propDesc = Object.getOwnPropertyDescriptor(obj, 'assigned');
-    expect(propDesc.writable).toBe(true);
-  });
-
+test('assertExists', () => {
+  expect(() => {Rac.utils.assertExists();})
+    .not.toThrow();
+  expect(() => {Rac.utils.assertExists("one", "two");})
+    .not.toThrow();
+  expect(() => {Rac.utils.assertExists("one", null, "three");})
+    .toThrow('FailedAssert');
 });
+
+
+test('typeName', () => {
+  let Duck = class Duck {};
+  let duck = new Duck();
+
+  expect(Rac.utils.typeName(Duck))
+    .toBe('function:Duck');
+  expect(Rac.utils.typeName(duck))
+    .toBe('Duck');
+
+  let namedFunc = function named() {};
+  expect(Rac.utils.typeName(namedFunc))
+    .toBe('function:named');
+
+  let unnamedFunc = function() {};
+  expect(Rac.utils.typeName(unnamedFunc))
+    .toBe('function:unnamedFunc');
+
+  let anonymousFunc = () => {};
+  expect(Rac.utils.typeName(anonymousFunc))
+    .toBe('function:anonymousFunc');
+
+  expect(Rac.utils.typeName(() => {}))
+    .toBe('function');
+
+  expect(Rac.utils.typeName(55))
+    .toBe('Number');
+  expect(Rac.utils.typeName('string'))
+    .toBe('String');
+});
+
+
+test('addConstant', () => {
+  let obj = {};
+  expect(obj).not.toHaveProperty('prop');
+
+  Rac.utils.addConstant(obj, 'prop', 'value');
+  expect(obj).toHaveProperty('prop', 'value');
+
+  let propDesc = Object.getOwnPropertyDescriptor(obj, 'prop');
+  expect(propDesc.writable).toBe(false);
+
+  obj.assigned = 'other';
+  expect(obj).toHaveProperty('assigned', 'other');
+  propDesc = Object.getOwnPropertyDescriptor(obj, 'assigned');
+  expect(propDesc.writable).toBe(true);
+});
+
