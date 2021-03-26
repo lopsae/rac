e84f4a8cb9391df664af74db5c51d6f54ab3dd1b
Maic Lopez Saenz <maic@lopsae.com>
2021-03-26 03:12:54 -0700

    rename makeArc to Arc

--- src/Rac.js
+++ src/Rac.js
@@ -230,7 +230,7 @@ Rac.setupDrawableProtoFunctions(Rac.Segment);
 
 
 // Arc
-Rac.Arc = require('./drawable/makeArc')(Rac);
+Rac.Arc = require('./drawable/Arc')(Rac);
 Rac.setupDrawableProtoFunctions(Rac.Arc);
 
 
