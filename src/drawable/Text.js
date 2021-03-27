f9dd69707606aca1335b8916f7490b528e48f9ee
Maic Lopez Saenz <maic@lopsae.com>
2021-03-27 14:14:56 -0700

    adjust bulk of Text indentation

--- src/drawable/Text.js
+++ src/drawable/Text.js
@@ -7,83 +7,83 @@ const utils = require('../util/utils');
 
 module.exports = function makeText(rac) {
 
-  class RacTextFormat {
+class RacTextFormat {
 
-    static defaultSize = 15;
+  static defaultSize = 15;
 
-    static horizontal = {
-      left: "left",
-      center: "horizontalCenter",
-      right: "right"
-    };
+  static horizontal = {
+    left: "left",
+    center: "horizontalCenter",
+    right: "right"
+  };
 
-    static vertical = {
-      top: "top",
-      bottom: "bottom",
-      center: "verticalCenter",
-      baseline: "baseline"
-    };
+  static vertical = {
+    top: "top",
+    bottom: "bottom",
+    center: "verticalCenter",
+    baseline: "baseline"
+  };
 
-    constructor(
+  constructor(
+    horizontal, vertical,
+    font = null,
+    rotation = rac.Angle.zero,
+    size = RacTextFormat.defaultSize)
+  {
+    this.horizontal = horizontal;
+    this.vertical = vertical;
+    this.font = font;
+    this.rotation = rotation;
+    this.size = size;
+  }
+
+  // Returns a format to draw text in the same position as `self` with
+  // the inverse angle.
+  inverse() {
+    let hEnum = RacTextFormat.horizontal;
+    let vEnum = RacTextFormat.vertical;
+    let horizontal, vertical;
+    switch (this.horizontal) {
+      case hEnum.left:
+        horizontal = hEnum.right; break;
+      case hEnum.right:
+        horizontal = hEnum.left; break;
+      default:
+        horizontal = this.horizontal; break;
+    }
+    switch (this.vertical) {
+      case vEnum.top:
+        vertical = vEnum.bottom; break;
+      case vEnum.bottom:
+        vertical = vEnum.top; break;
+      default:
+        vertical = this.vertical; break;
+    }
+
+    return new RacTextFormat(
       horizontal, vertical,
-      font = null,
-      rotation = rac.Angle.zero,
-      size = RacTextFormat.defaultSize)
-    {
-      this.horizontal = horizontal;
-      this.vertical = vertical;
-      this.font = font;
-      this.rotation = rotation;
-      this.size = size;
-    }
+      this.font,
+      this.rotation.inverse(),
+      this.size)
+  }
 
-    // Returns a format to draw text in the same position as `self` with
-    // the inverse angle.
-    inverse() {
-      let hEnum = RacTextFormat.horizontal;
-      let vEnum = RacTextFormat.vertical;
-      let horizontal, vertical;
-      switch (this.horizontal) {
-        case hEnum.left:
-          horizontal = hEnum.right; break;
-        case hEnum.right:
-          horizontal = hEnum.left; break;
-        default:
-          horizontal = this.horizontal; break;
-      }
-      switch (this.vertical) {
-        case vEnum.top:
-          vertical = vEnum.bottom; break;
-        case vEnum.bottom:
-          vertical = vEnum.top; break;
-        default:
-          vertical = this.vertical; break;
-      }
-
-      return new RacTextFormat(
-        horizontal, vertical,
-        this.font,
-        this.rotation.inverse(),
-        this.size)
-    }
-
-  } // RacTextFormat
+} // RacTextFormat
 
 
-  class RacText {
+class RacText {
 
-    constructor(string, format, point) {
-      this.string = string;
-      this.format = format;
-      this.point = point;
-    }
+  constructor(string, format, point) {
+    this.string = string;
+    this.format = format;
+    this.point = point;
+  }
 
-    static Format = RacTextFormat;
+  static Format = RacTextFormat;
 
-  } // RacText
+} // RacText
 
 
-  return RacText;
+return RacText;
 
 } // makeText
 
