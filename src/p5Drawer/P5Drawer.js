02a12e995652a19f66fd98b661d45f2be2509380
Maic Lopez Saenz <maic@lopsae.com>
2021-03-28 01:36:12 -0700

    start moving draw functions to its own file
    + Point, Segment and Arc functions moved

--- src/p5Drawer/P5Drawer.js
+++ src/p5Drawer/P5Drawer.js
@@ -183,11 +183,11 @@ class P5Drawer {
   // Also attaches additional prototype and static functions in relevant
   // classes.
   setupAllDrawFunctions(rac) {
+    let functions = require('./draw.functions');
+
     // Point
+    this.setDrawFunction(Rac.Point, functions.drawPoint);
     require('./Point.functions')(rac);
-    this.setDrawFunction(Rac.Point, (drawer, point) => {
-      drawer.p5.point(point.x, point.y);
-    });
 
     // Ray
     this.setDrawFunction(Rac.Ray, (drawer, ray) => {
@@ -196,37 +196,11 @@ class P5Drawer {
     });
 
     // Segment
+    this.setDrawFunction(Rac.Segment, functions.drawSegment);
     require('./Segment.functions')(rac);
-    this.setDrawFunction(Rac.Segment, (drawer, segment) => {
-      drawer.p5.line(
-        segment.start.x, segment.start.y,
-        segment.end.x,   segment.end.y);
-    });
 
     // Arc
-    this.setDrawFunction(Rac.Arc, (drawer, arc) => {
-      if (arc.isCircle()) {
-        let startRad = arc.start.radians();
-        let endRad = startRad + (Rac.TAU);
-        drawer.p5.arc(
-          arc.center.x, arc.center.y,
-          arc.radius * 2, arc.radius * 2,
-          startRad, endRad);
-        return;
-      }
-
-      let start = arc.start;
-      let end = arc.end;
-      if (!arc.clockwise) {
-        start = arc.end;
-        end = arc.start;
-      }
-
-      drawer.p5.arc(
-        arc.center.x, arc.center.y,
-        arc.radius * 2, arc.radius * 2,
-        start.radians(), end.radians());
-    });
+    this.setDrawFunction(Rac.Arc, functions.drawArc);
 
     Rac.Arc.prototype.vertex = function() {
       let angleDistance = this.angleDistance();
---
+++ src/p5Drawer/draw.functions.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+exports.drawPoint = function(drawer, point) {
+  drawer.p5.point(point.x, point.y);
+}; // drawPoint
+
+
+exports.drawSegment = function(drawer, segment) {
+  drawer.p5.line(
+    segment.start.x, segment.start.y,
+    segment.end.x,   segment.end.y);
+}; // drawSegment
+
+
+exports.drawArc = function(drawer, arc) {
+  if (arc.isCircle()) {
+    let startRad = arc.start.radians();
+    let endRad = startRad + (Rac.TAU);
+    drawer.p5.arc(
+      arc.center.x, arc.center.y,
+      arc.radius * 2, arc.radius * 2,
+      startRad, endRad);
+    return;
+  }
+
+  let start = arc.start;
+  let end = arc.end;
+  if (!arc.clockwise) {
+    start = arc.end;
+    end = arc.start;
+  }
+
+  drawer.p5.arc(
+    arc.center.x, arc.center.y,
+    arc.radius * 2, arc.radius * 2,
+    start.radians(), end.radians());
+}; // drawArc
+
