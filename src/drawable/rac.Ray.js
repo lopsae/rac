02d4cd8e4f9f6074450ffd7fcc3ef7e030255e21
Maic Lopez Saenz <maic@lopsae.com>
2021-04-14 21:28:47 -0700

    add instance.Ray.zero

--- src/drawable/rac.Ray.js
+++ src/drawable/rac.Ray.js
@@ -9,8 +9,24 @@
 module.exports = function attachRacRay(rac) {
 
   /**
-  * A `Ray` over the x-axis, starting at `{@link instance.Point#origin}`
-  * and poiting to `{@link instance.Angle#zero}`.
+  * A `Ray` with all values set to zero, starts at
+  * `{@link instance.Point#zero}` and points to
+  * `{@link instance.Angle#zero}`.
+  *
+  * @name zero
+  * @type {Rac.Ray}
+  * @memberof instance.Ray#
+  * @see instance.Point#zero
+  * @see instance.Angle#zero
+  */
+  rac.Ray.zero = rac.Ray(0, 0, rac.Angle.zero);
+
+
+  /**
+  * A `Ray` over the x-axis, starts at `{@link instance.Point#origin}` and
+  * points to `{@link instance.Angle#zero}`.
+  *
+  * Equal to `{@link instance.Ray#zero}`.
   *
   * @name xAxis
   * @type {Rac.Ray}
@@ -18,12 +34,12 @@ module.exports = function attachRacRay(rac) {
   * @see instance.Point#origin
   * @see instance.Angle#zero
   */
-  rac.Ray.xAxis = rac.Ray(0, 0, rac.Angle.zero);
+  rac.Ray.xAxis = rac.Ray.zero;
 
 
   /**
-  * A `Ray` over the y-axis, starting at`{@link instance.Point.origin}`
-  * and poiting to `{@link instance.Angle.quarter}`.
+  * A `Ray` over the y-axis, starts at`{@link instance.Point.origin}` and
+  * points to `{@link instance.Angle.quarter}`.
   *
   * @name yAxis
   * @type {Rac.Ray}
