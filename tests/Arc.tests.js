63ad19b53a17d25f45f769f7dfa8a6ca9ae830b6
Maic Lopez Saenz <maic@lopsae.com>
2021-04-20 23:34:08 -0700

    add Arc tests for Unexpected type for equals

--- tests/Arc.tests.js
+++ tests/Arc.tests.js
@@ -10,7 +10,7 @@ const rac = tools.rac;
 const half = rac.Arc(100, 100, 55, rac.Angle.up, rac.Angle.down);
 
 
-test('Identity', () => {
+tools.test( function identity() {
   // Instance members
   expect(rac.Arc.zero).equalsArc(0, 0, 0, 0, 0, true);
   expect(half).equalsArc(100, 100, 55, 3/4, 1/4, true);
@@ -36,6 +36,15 @@ test('Identity', () => {
   expect(rac.Ray.zero)    .not.equalsArc(0, 0, 0, 0, 0, true);
   expect(rac.Segment.zero).not.equalsArc(0, 0, 0, 0, 0, true);
 
+  // Unexpected type for equals
+  expect(half.equals(null))            .toBe(false);
+  expect(half.equals(0))               .toBe(false);
+  expect(half.equals('0'))             .toBe(false);
+  expect(half.equals(rac.Angle.zero))  .toBe(false);
+  expect(half.equals(rac.Point.zero))  .toBe(false);
+  expect(half.equals(rac.Ray.zero))    .toBe(false);
+  expect(half.equals(rac.Segment.zero)).toBe(false);
+
 
   let string = half.toString();
   expect(string).toMatch('Arc');
@@ -45,20 +54,6 @@ test('Identity', () => {
   expect(string).toMatch('0.75');
   expect(string).toMatch('true');
 });
-
-
-// tools.test( function identity() {
-
-
-
-//   // Unexpected type for equals
-//   expect(hunty.equals(null))            .toBe(false);
-//   expect(hunty.equals(100))             .toBe(false);
-//   expect(hunty.equals('100'))           .toBe(false);
-//   expect(hunty.equals(rac.Angle.zero))  .toBe(false);
-//   expect(hunty.equals(rac.Ray.zero))    .toBe(false);
-//   expect(hunty.equals(rac.Segment.zero)).toBe(false);
-// });
 
 
 test.todo('Check for coverage!');
