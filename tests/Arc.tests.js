2ca87a9a2d68c66a1fc82712d117def74311f305
Maic Lopez Saenz <maic@lopsae.com>
2021-04-20 23:30:35 -0700

    add angle/number tests to Arc
    + along Unexpected type for equalsRay

--- tests/Arc.tests.js
+++ tests/Arc.tests.js
@@ -7,12 +7,17 @@ const tools = require('./tools');
 const rac = tools.rac;
 
 
-let half = rac.Arc(100, 100, 55, rac.Angle.up, rac.Angle.down);
+const half = rac.Arc(100, 100, 55, rac.Angle.up, rac.Angle.down);
 
 
 test('Identity', () => {
   // Instance members
   expect(rac.Arc.zero).equalsArc(0, 0, 0, 0, 0, true);
+  expect(half).equalsArc(100, 100, 55, 3/4, 1/4, true);
+
+  // Angle/number parameter
+  expect(half).equalsArc(100, 100, 55, 3/4, 1/4, true);
+  expect(half).equalsArc(100, 100, 55, rac.Angle.up, rac.Angle.down, true);
 
   // Inequality
   expect(rac.Arc.zero).not.equalsArc(7, 0, 0, 0.0, 0.0, true);
@@ -20,12 +25,17 @@ test('Identity', () => {
   expect(rac.Arc.zero).not.equalsArc(0, 0, 7, 0.0, 0.0, true);
   expect(rac.Arc.zero).not.equalsArc(0, 0, 0, 0.7, 0.0, true);
   expect(rac.Arc.zero).not.equalsArc(0, 0, 0, 0.0, 0.7, true);
-  expect(rac.Arc.zero).not.equalsArc(0, 0, 0, 0.0, 0.0, true);
+  expect(rac.Arc.zero).not.equalsArc(0, 0, 0, 0.0, 0.0, false);
 
-  expect(null).not.equalsArc(0, 0, 100, 0, 1/2, true);
+  // Unexpected type for equalsRay
+  expect(null)            .not.equalsArc(0, 0, 0, 0, 0, true);
+  expect(0)               .not.equalsArc(0, 0, 0, 0, 0, true);
+  expect('0')             .not.equalsArc(0, 0, 0, 0, 0, true);
+  expect(rac.Angle.zero)  .not.equalsArc(0, 0, 0, 0, 0, true);
+  expect(rac.Point.zero)  .not.equalsArc(0, 0, 0, 0, 0, true);
+  expect(rac.Ray.zero)    .not.equalsArc(0, 0, 0, 0, 0, true);
+  expect(rac.Segment.zero).not.equalsArc(0, 0, 0, 0, 0, true);
 
-  expect(half).equalsArc(100, 100, 55, rac.Angle.up, rac.Angle.down, true);
-  expect(half).not.equalsArc(100, 100, 55, rac.Angle.up, rac.Angle.down, false);
 
   let string = half.toString();
   expect(string).toMatch('Arc');
@@ -35,6 +45,20 @@ test('Identity', () => {
   expect(string).toMatch('0.75');
   expect(string).toMatch('true');
 });
+
+
+// tools.test( function identity() {
+
+
+
+//   // Unexpected type for equals
+//   expect(hunty.equals(null))            .toBe(false);
+//   expect(hunty.equals(100))             .toBe(false);
+//   expect(hunty.equals('100'))           .toBe(false);
+//   expect(hunty.equals(rac.Angle.zero))  .toBe(false);
+//   expect(hunty.equals(rac.Ray.zero))    .toBe(false);
+//   expect(hunty.equals(rac.Segment.zero)).toBe(false);
+// });
 
 
 test.todo('Check for coverage!');
