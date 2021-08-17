75d7b66f0426bcca0bc51077f02fa60b033dc582
Maic Lopez Saenz <maic@lopsae.com>
2021-08-17 16:24:54 -0700

    add documentation to Color

--- src/style/Color.js
+++ src/style/Color.js
@@ -6,12 +6,21 @@ const utils = require('../util/utils');
 
 
 /**
-* Color with RBGA values, each on the `[0,1]` range.
+* Color with RBGA values, each one on the *[0,1]* range.
 *
 * @alias Rac.Color
 */
 class Color {
 
+  /**
+  * Creates a new `Color` instance.
+  *
+  * @param {Rac} rac - Instance to use for drawing and creating other objects
+  * @param {number} r - The red channel value, in the *[0,1]* range
+  * @param {number} g - The green channel value, in the *[0,1]* range
+  * @param {number} b - The blue channel value, in the *[0,1]* range
+  * @param {number} [alpha=1] - The alpha channel value, in the *[0,1]* range
+  */
   constructor(rac, r, g, b, alpha = 1) {
     utils.assertExists(rac, r, g, b, alpha);
     utils.assertNumber(r, g, b, alpha);
@@ -22,29 +31,73 @@ class Color {
     this.alpha = alpha;
   }
 
+
   /**
   * Returns a string representation intended for human consumption.
+  *
+  * @returns {string}
   */
   toString() {
     return `Color(${this.r},${this.g},${this.b},${this.alpha})`;
   }
 
+
+  /**
+  * Creates a new `Color` instance with each channel received in the
+  * *[0,255]* range
+  *
+  * @param {Rac} rac - Instance to use for drawing and creating other objects
+  * @param {number} r - The red channel value, in the *[0,255]* range
+  * @param {number} g - The green channel value, in the *[0,255]* range
+  * @param {number} b - The blue channel value, in the *[0,255]* range
+  * @param {number} [alpha=255] - The alpha channel value, in the *[0,255]* range
+  *
+  * @returns {Rac.Color}
+  */
   static fromRgba(rac, r, g, b, a = 255) {
     return new Color(rac, r/255, g/255, b/255, a/255);
   }
 
+
+  /**
+  * Returns a new `Fill` that uses `this` as `color`.
+  *
+  * @returns {Rac.Fill}
+  */
   fill() {
     return new Rac.Fill(this.rac, this);
   }
 
+
+  /**
+  * Returns a new `Stroke` that uses `this` as `color`.
+  *
+  * @param {?number} weight - The weight of the new `Stroke`
+  * @returns {Rac.Stroke}
+  */
   stroke(weight = null) {
     return new Rac.Stroke(this.rac, weight, this);
   }
 
+
+  /**
+  * Returns a new `Color` with `alpha` set to `newAlpha`.
+  *
+  * @param {number} newAlpha - The alpha channel for the new `Color`, in the
+  *   *[0,1]* range
+  * @returns {Rac.Color}
+  */
   withAlpha(newAlpha) {
     return new Color(this.rac, this.r, this.g, this.b, newAlpha);
   }
 
+
+  /**
+  * Returns a new `Color` with `alpha` set to `this.alpha * ratio`.
+  *
+  * @param {number} ratio - The factor to multiply `alpha` by
+  * @returns {Rac.Color}
+  */
   withAlphaRatio(ratio) {
     return new Color(this.rac, this.r, this.g, this.b, this.alpha * ratio);
   }
