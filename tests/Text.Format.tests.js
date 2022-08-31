abaeb93282fed15426abc7bcbf1087d09ca1361e
Maic Lopez Saenz <maic@lopsae.com>
2022-08-31 14:38:25 -0700

    add more identity tests to Text.Format

--- tests/Text.Format.tests.js
+++ tests/Text.Format.tests.js
@@ -10,26 +10,29 @@ const rac = tools.rac;
 const ha = Rac.Text.Format.horizontalAlign;
 const va = Rac.Text.Format.verticalAlign;
 
+const centered = rac.Text.Format(ha.center, va.center);
+
 
 test('Identity', () => {
   // Rac instance
   const otherRac = new Rac();
   expect(otherRac.Text.Format.centerCenter)
     .not.equalsTextFormat(ha.center, va.center, 0, null, null);
-
   expect(rac.Text.Format.centerCenter).equalsTextFormat(ha.center, va.center, 0, null, null);
   expect(otherRac.Text.Format.centerCenter.equals(rac.Text.Format.centerCenter))
     .toBe(true);
 
   // Instance members
-  // expect(rac.Point.zero).equalsPoint(0, 0);
-  // expect(rac.Point.origin).equalsPoint(0, 0);
-  // expect(hunty).equalsPoint(100, 100);
-  // expect(fifty).equalsPoint(55, 55);
+  expect(rac.Text.Format.topLeft).equalsTextFormat(ha.left, va.top, 0, null, null);
+  expect(rac.Text.Format.centerCenter).equalsTextFormat(ha.center, va.center, 0, null, null);
+  expect(centered).equalsTextFormat(ha.center, va.center, 0, null, null);
 
   // Inequality
-  // expect(rac.Point.zero).not.equalsPoint(7, 0);
-  // expect(rac.Point.zero).not.equalsPoint(0, 7);
+  expect(centered).not.equalsTextFormat(ha.left,   va.center, 0,   null,  null);
+  expect(centered).not.equalsTextFormat(ha.center, va.top,    0,   null,  null);
+  expect(centered).not.equalsTextFormat(ha.center, va.center, 1/2, null,  null);
+  expect(centered).not.equalsTextFormat(ha.center, va.center, 0,  'sans', null);
+  expect(centered).not.equalsTextFormat(ha.center, va.center, 0,   null,  10);
 
   // Unexpected type for equalsPoint
   // expect(null)            .not.equalsPoint(0, 0);
