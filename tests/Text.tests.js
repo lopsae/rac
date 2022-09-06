e9fb7fb82dceb5ad961dfd1f8043f006ce1dc8e8
Maic Lopez Saenz <maic@lopsae.com>
2022-09-06 12:26:17 -0700

    update Text identity tests

--- tests/Text.tests.js
+++ tests/Text.tests.js
@@ -7,27 +7,68 @@ const tools = require('./tools');
 const rac = tools.rac;
 
 
-const wizards = rac.Text(55, 55, 'The five boxing wizards jump quickly',
+const zebras = rac.Text(55, 55, 'daft zebras',
   rac.Text.Format.centered);
-const example = rac.Text(100, 100, 'example');
+const quartz = rac.Text(100, 100, 'black quartz');
 // RELEASE-TODO: test that equalsText is ignoring format
 
+const ha = Rac.Text.Format.horizontalAlign;
+const va = Rac.Text.Format.verticalAlign;
 
-test('Identity', () => {
+
+tools.test( function identity() {
   // Rac instance
   const otherRac = new Rac();
   expect(otherRac.Text.sphinx).not.equalsText(0, 0, 'sphinx of black quartz, judge my vow');
   expect(rac.Text.sphinx)         .equalsText(0, 0, 'sphinx of black quartz, judge my vow');
 
-  expect(null).not.equalsText(0, 0, "Identity");
+  expect(otherRac.Text.sphinx.equals(rac.Text.sphinx)).toBe(true);
 
-  expect(wizards).equalsText(55, 55, 'The five boxing wizards jump quickly');
-  expect(wizards).not.equalsText(7, 7, 'The five boxing wizards jump quickly');
+  // Testing Constants
+  expect(zebras).equalsText(55, 55, 'daft zebras');
+  expect(zebras.format).equalsTextFormat(ha.center, va.center);
 
-  let string = wizards.toString();
-  expect(string).toMatch('Text');
-  expect(string).toMatch('55,55');
-  expect(string).toMatch('The five boxing wizards jump quickly');
+  expect(quartz).equalsText(100, 100, 'black quartz');
+  expect(quartz.format).equalsTextFormat(ha.left, va.top);
+
+  // Inequality
+  expect(zebras).not.equalsText(0,  55, 'daft zebras');
+  expect(zebras).not.equalsText(55, 0,  'daft zebras');
+  expect(zebras).not.equalsText(55, 55, 'boxing wizards');
+
+  // Unexpected type for equalsText
+  expect(null)                    .not.equalsText(0, 0, '');
+  expect(0)                       .not.equalsText(0, 0, '');
+  expect('')                      .not.equalsText(0, 0, '');
+  expect('0')                     .not.equalsText(0, 0, '');
+  expect(55)                      .not.equalsText(0, 0, '');
+  expect('55')                    .not.equalsText(0, 0, '');
+  expect('some')                  .not.equalsText(0, 0, '');
+  expect(true)                    .not.equalsText(0, 0, '');
+  expect(false)                   .not.equalsText(0, 0, '');
+  expect(rac.Angle.zero)          .not.equalsText(0, 0, '');
+  expect(rac.Ray.zero)            .not.equalsText(0, 0, '');
+  expect(rac.Segment.zero)        .not.equalsText(0, 0, '');
+  expect(rac.Text.Format.centered).not.equalsText(0, 0, '');
+
+  // Expected type for equals
+  expect(quartz.equals(quartz)).toBe(true);
+  expect(quartz.equals(zebras)).toBe(false);
+
+  // Unexpected type for equals
+  expect(zebras.equals(null))                    .toBe(false);
+  expect(zebras.equals(''))                      .toBe(false);
+  expect(zebras.equals(0))                       .toBe(false);
+  expect(zebras.equals('0'))                     .toBe(false);
+  expect(zebras.equals(55))                      .toBe(false);
+  expect(zebras.equals('55'))                    .toBe(false);
+  expect(zebras.equals('some'))                  .toBe(false);
+  expect(zebras.equals(true))                    .toBe(false);
+  expect(zebras.equals(false))                   .toBe(false);
+  expect(zebras.equals(rac.Angle.zero))          .toBe(false);
+  expect(zebras.equals(rac.Point.zero))          .toBe(false);
+  expect(zebras.equals(rac.Ray.zero))            .toBe(false);
+  expect(zebras.equals(rac.Text.Format.centered)).toBe(false);
 });
 
 
@@ -42,6 +83,12 @@ tools.test( function toString() {
   expect(text.toString()) .toBe('Text((1.12345,2.12345) "judge my vow")');
   expect(text.toString(2)).toBe('Text((1.12,2.12) "judge my vow")');
 });
+
+
+// Testing instance members
+// Testing Constants
+  // expect(rac.Text.hello).not.equalsText(0, 0, 'hello world!');
+  // expect(rac.Text.sphinx).not.equalsText(0, 0, 'sphinx of black quartz, judge my vow');
 
 
 // RELEASE-TODO: test!
