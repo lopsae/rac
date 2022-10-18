da9d977c6abf29121c3d7e1911b06ba2f429442c
Maic Lopez Saenz <maic@lopsae.com>
2022-10-18 13:25:56 -0700

    commit built version.js
    + so that it contains the `dated` property needed for tests

--- built/version.js
+++ built/version.js
@@ -2,7 +2,8 @@
 
 // Ruler and Compass - version and build
 module.exports = {
-	version: '1.2.0',
-	build: '1005-94b722f'
+	version: '1.2.1-dev',
+	build:   'localBuild-1092-9c1208d',
+	dated:   '2022-10-18T20:24:57.718Z'
 };
 
