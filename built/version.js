add28b79f6b3c5f0ae1d527f5345e86f360f1133
Maic Lopez Saenz <maic@lopsae.com>
2022-10-24 15:22:17 -0700

    rebuild version.js to include it in docs

--- built/version.js
+++ built/version.js
@@ -1,9 +1,34 @@
 'useStrict';
 
 // Ruler and Compass - version and build
+/**
+* Container of the version data for the package.
+* @namespace version
+*/
 module.exports = {
-	version: '1.2.1-dev',
-	build:   'localBuild-1092-9c1208d',
-	dated:   '2022-10-18T20:24:57.718Z'
+
+  /**
+  * Version of the package. Exposed through
+  * [`Rac.version`]{@link Rac.version}.
+  * @constant {String} version
+  * @memberof version#
+  */
+  version: '1.2.1-dev',
+
+  /**
+  * Build of the package. Exposed through
+  * [`Rac.build`]{@link Rac.build}.
+  * @constant {String} build
+  * @memberof version#
+  */
+  build: 'localBuild-1132-32ffddb',
+
+  /**
+  * Date of build of the package. Exposed through
+  * [`Rac.dated`]{@link Rac.dated}.
+  * @constant {String} dated
+  * @memberof version#
+  */
+  dated: '2022-10-24T22:07:19.749Z'
 };
 
