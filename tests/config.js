78bf75e38004f724460b4c47ae989dffae140cf7
Maic Lopez Saenz <maic@lopsae.com>
2023-09-27 17:02:03 -0700

    add test coverage settings for some files

--- tests/config.js
+++ tests/config.js
@@ -1,20 +1,43 @@
 
-const config = {
+let config = {
   // verbose: true,
   rootDir: '../',
   testMatch: [
       '**/tests/*.tests.js'],
   coverageThreshold: {
       global: {
-        branches: 50,
-        functions: 50,
-        lines: 100,
-        statements: 50
+        branches: 40,
+        functions: 40,
+        lines: 40,
+        statements: 40
       },
       './src/drawable/*.js': {
-        lines: 100
+        lines: 50
       }
     }
 };
+
+// Complete Coverage Files
+[
+  './src/drawable/Angle.js',
+  './src/drawable/Point.js',
+  // './src/drawable/Ray.js',
+  './src/drawable/Segment.js',
+  './src/drawable/Arc.js',
+  './src/drawable/Text.js',
+  './src/drawable/Text.Format.js',
+  './src/drawable/instance.Angle.js',
+  './src/drawable/instance.Point.js',
+  './src/drawable/instance.Ray.js',
+  './src/drawable/instance.Segment.js',
+  './src/drawable/instance.Arc.js',
+  './src/drawable/instance.Bezier.js',
+  './src/drawable/instance.Text.js',
+].forEach(item => {
+  config.coverageThreshold[item] = {lines: 100};
+});
+
+config.coverageThreshold['./src/drawable/Ray.js'] = {lines: 80};
+
 
 module.exports = config;
